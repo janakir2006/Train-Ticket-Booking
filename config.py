afa1e282d0923a69e782a44b39b8d75e6d0b1a8d
Jankaki R <janakir2006@gmail.com>
2026-02-15 11:15:10 +0530

    Updated database config for  Render

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 
 class Config:
-    # Format: postgresql://username:password@localhost:port/database_name
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:jan1558@localhost:5432/flaskapp'
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
